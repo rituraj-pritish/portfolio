b8cc9da74b0e92e272f3eb2241c65b593fc3ca7e
rituraj-pritish <riturajpritish@gmail.com>
2020-02-18 15:14:08 +0530

    Updates

---
+++ main.js
@@ -0,0 +1,38 @@
+const smoothScrollHandler = target => {
+  const pos = target.getBoundingClientRect();
+
+  window.scroll({
+    top: pos.y + 5,
+    left: 0,
+    behavior: 'smooth'
+  });
+};
+const projects = document.getElementById('projects');
+const viewMyWorkBtn = document.getElementById('view-my-work-btn');
+
+viewMyWorkBtn.addEventListener('click', () => smoothScrollHandler(projects));
+
+const nav = document.querySelector('.nav');
+const transparentNav = document.querySelector('.nav-transparent');
+const coloredNav = document.querySelector('.nav-colored');
+const header = document.querySelector('.header');
+const headerHeight = header.scrollHeight;
+
+const navScrollHandler = () => {
+  const scrollOffset = window.pageYOffset;
+
+  if (scrollOffset > headerHeight) {
+    transparentNav.style.display = 'none';
+    coloredNav.style.display = 'flex';
+    nav.style.borderColor = 'grey';
+  }
+
+  if (scrollOffset < headerHeight) {
+    transparentNav.style.display = 'flex';
+    coloredNav.style.display = 'none';
+    nav.style.borderColor = '#d8d8d8';
+  }
+};
+navScrollHandler();
+
+document.addEventListener('scroll', navScrollHandler);
